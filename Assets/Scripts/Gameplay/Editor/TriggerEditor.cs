d4523d4ed0c5b4a750e86571a8046635fed4b1ff
Alex Hajdu <alex.hajdu@inloop.eu>
2016-11-17 17:01:21 +0100

    - Editor for tiles - Triggerer as prefab

---
+++ Assets/Scripts/Gameplay/Editor/TriggerEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor ( typeof ( EnvTrigger ) )]
+public class TriggerEditor : Editor
+{
+    EnvTrigger _myTarget;
+
+    void OnEnable ( )
+    {
+        _myTarget = ( EnvTrigger ) target;
+    }
+
+    public override void OnInspectorGUI ( )
+    {
+        base.OnInspectorGUI ( );
+        _myTarget.size = _myTarget.GetComponent<Collider> ( ).bounds.size;
+    }
+}
---
+++ Assets/Scripts/Gameplay/TileBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent ( typeof ( BoxCollider ) )]
+public class TileBase : MonoBehaviour
+{
+    [SerializeField]
+    private bool _randomizeOnStart = true;
+
+    public float yScaleRatio = 0.6f;
+
+    void Start ( )
+    {
+        if ( _randomizeOnStart ) RandomizeInitRotation ( );
+    }
+
+    private void RandomizeInitRotation ( )
+    {
+        transform.Rotate ( Vector3.up, Random.Range ( 0, 3 ) * 90, Space.World );
+    }
+}
