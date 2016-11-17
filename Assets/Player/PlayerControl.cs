7544b0780192e99e2e1b5d8c34dc0fc1785d0eea
Peter Kendra <kendra@inloop.eu>
2016-11-17 17:19:41 +0100

    Player auto moving switch

--- Assets/Player/PlayerControl.cs
+++ Assets/Player/PlayerControl.cs
@@ -4,37 +4,36 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerControl : MonoBehaviour
 {
-	public float Speed = 5;
+	bool isAutoMoving = true;
+	float Speed = 5;
 	Vector3 JumpVector = new Vector3(0, 20, 0);
 	Vector3 Gravity = new Vector3(0, -1, 0);
-
 	CharacterController controller;
-	private Vector3 moveDirection = Vector3.zero;
-
-	private Vector3 startPosition;
+	Vector3 moveDirection = Vector3.zero;
 
 	// Use this for initialization
 	void Start()
 	{
-		startPosition = transform.position;
 		controller = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-		if (controller.isGrounded) {
-			moveDirection = new Vector3(1, 0, Input.GetAxis("Horizontal"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= Speed;
+		if (isAutoMoving) {
+			if (controller.isGrounded) {
+				moveDirection = new Vector3(1, 0, Input.GetAxis("Horizontal"));
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= Speed;
 
-			if (Input.GetButton("Jump"))
-				moveDirection += JumpVector;
+				if (Input.GetButton("Jump"))
+					moveDirection += JumpVector;
 
+			}
+
+			moveDirection += Gravity;
+			controller.Move(moveDirection * Time.deltaTime);
 		}
-
-		moveDirection += Gravity;
-		controller.Move(moveDirection * Time.deltaTime);
 	}
 
 	void OnControllerColliderHit(ControllerColliderHit hit) {
@@ -47,5 +46,12 @@ public class PlayerControl : MonoBehaviour
 
 		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
 		body.velocity = pushDir * 10;
+	}
+
+	public void setAutoMoving(bool value) {
+		if (isAutoMoving && !value) {
+			moveDirection = Vector3.zero;
+		}
+		isAutoMoving = value;
 	}
 }
