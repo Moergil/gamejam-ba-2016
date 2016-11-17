cee8609751567db62d1f4d501e424806c927e6db
Peter Kendra <kendra@inloop.eu>
2016-11-17 21:05:46 +0100

    Changes

---
+++ Assets/Player/PlayerBounds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBounds : MonoBehaviour {
+	public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = Target.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3(Target.position.x, transform.position.y, transform.position.z);
+	}
+}
