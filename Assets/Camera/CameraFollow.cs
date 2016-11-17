dadeaf03518dff198f71e1081d66af5a7c90903a
Peter Kendra <kendra@inloop.eu>
2016-11-17 20:36:57 +0100

    camera and prefabs

--- Assets/Camera/CameraFollow.cs
+++ Assets/Camera/CameraFollow.cs
@@ -5,15 +5,15 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour
 {
 	public Transform Target;
-	public Vector3 Offset;
-
+	public Vector3 Offset = new Vector3(-2.41f, 4.13f, -4.1f);
+	public Vector3 Rotation = new Vector3(30.631f, 54.883f, 3.34f);
 	private Vector3 velocity = Vector3.zero;
 
 	// Use this for initialization
 	void Start()
 	{
-		Offset = transform.position - Target.position;
 		transform.position = normalizedTargetObjectPosition();
+		transform.rotation = Quaternion.Euler (Rotation);
 	}
 	
 	// Update is called once per frame
