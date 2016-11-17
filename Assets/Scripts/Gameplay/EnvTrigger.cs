3401ba0865bbc83f35fbdd5353100186ee137473
Alex Hajdu <alex.hajdu@inloop.eu>
2016-11-17 14:30:00 +0100

    - Basic Scene management - Basic Trigger idea implemented

---
+++ Assets/Scripts/Gameplay/EnvTrigger.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using ByteSheep.Events;
+
+[System.Serializable]
+public class EnvCollisionEvent : AdvancedEvent<ObstacleBase> { }
+
+public class EnvTrigger : MonoBehaviour
+{
+    public EnvCollisionEvent TrgEvent;
+
+    private Collider _col;
+
+    #region Mono
+
+    void Awake ( )
+    {
+        _col = GetComponent<Collider> ( );
+
+        if ( _col != null )
+            _col.isTrigger = true;
+    }
+
+    #endregion
+
+    #region Trigger handlers
+
+    void OnTriggerEnter ( Collider col )
+    {
+        if ( col.tag == "Player" )
+        {
+            //  Fire event
+            TrgEvent.Invoke ( null );
+
+            // Destroy collider
+            Destroy ( _col );
+
+            Debug.Log ( "EnvTriger -> On Enter" );
+        }
+    }
+
+    #endregion
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube ( transform.position, Vector3.one);
+    }
+}
+
+
---
+++ Assets/Scripts/Obstacles/ObstacleBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleBase : MonoBehaviour
+{
+    public bool IsMoveable { get { return this is IIsMoveAble; } }
+
+    #region API
+
+    public virtual void Activate ( )
+    {
+        Debug.Log ( "ACTIVATE" );
+    }
+
+    #endregion
+}
