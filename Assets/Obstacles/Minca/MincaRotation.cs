178e4bc98bd44bad7f60e22e4892c134ef9d711c
Peter Kendra <kendra@inloop.eu>
2016-11-18 12:36:51 +0100

    mince

--- Assets/Obstacles/Minca/MincaRotation.cs
+++ Assets/Obstacles/Minca/MincaRotation.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class MincaRotation : MonoBehaviour {
-
+	float rotationSpeed = 0;
 	// Use this for initialization
 	void Start () {
-	
+		rotationSpeed = Random.Range(80,120);
+		transform.Rotate (Vector3.up, Random.Range(0,1024));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (Vector3.up, Time.deltaTime * 100);
+		transform.Rotate (Vector3.up, Time.deltaTime * rotationSpeed);
 	}
 }
